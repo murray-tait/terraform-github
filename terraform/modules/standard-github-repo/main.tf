a208545a3b8f4107ec02842b7cdfd4dd0c679b86
Murray Tait <6074789+deathtumble@users.noreply.github.com>
2025-11-29 20:34:21 +0000

    Add branch protection to module

--- terraform/modules/standard-github-repo/main.tf
+++ terraform/modules/standard-github-repo/main.tf
@@ -74,3 +74,62 @@ resource "github_actions_environment_variable" "this_main_plan_aws_role_to_assum
   variable_name = "AWS_ROLE_TO_ASSUME"
   value         = "arn:aws:iam::973963482762:role/Github-Actions-OIDC-murray-tait"
 }
+
+resource "github_branch_protection" "this_main" {
+  repository_id                   = github_repository.this.id
+  allows_deletions                = false
+  allows_force_pushes             = false
+  enforce_admins                  = false
+  lock_branch                     = false
+  pattern                         = "main"
+  require_conversation_resolution = true
+  require_signed_commits          = false
+  required_linear_history         = false
+  force_push_bypassers            = ["/murray-tait", ]
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    require_last_push_approval      = true
+    required_approving_review_count = 1
+
+  }
+}
+
+resource "github_repository_ruleset" "this_main" {
+  name        = "main"
+  repository  = github_repository.this.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+  }
+
+  bypass_actors {
+    actor_id    = 4
+    actor_type  = "RepositoryRole"
+    bypass_mode = "always"
+
+  }
+
+  rules {
+    creation                = true
+    update                  = true
+    deletion                = true
+    required_linear_history = false
+    required_signatures     = true
+
+    pull_request {
+      required_review_thread_resolution = true
+      require_last_push_approval        = true
+      required_approving_review_count   = 1
+    }
+  }
+}
