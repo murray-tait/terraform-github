03e47b93c11810565c73cd5a9e9d69cc1e55f4eb
Murray Tait <6074789+deathtumble@users.noreply.github.com>
2025-12-14 16:51:18 +0000

    Remove unused import blocks from parent module configuration

--- terraform/parent.tf
+++ terraform/parent.tf
@@ -4,13 +4,3 @@ module "parent_repository" {
   aws_role_to_assume = "arn:aws:iam::973963482762:role/Github-Actions-OIDC-murray-tait"
   review_user_ids    = [data.github_user.owner.id]
 }
-
-import {
-  id = "parent"
-  to = module.parent_repository.github_repository.this
-}
-
-import {
-  id = "parent:8456580"
-  to = module.parent_repository.github_repository_ruleset.this_main
-}
